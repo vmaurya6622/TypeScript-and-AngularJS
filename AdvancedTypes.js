c69546de28e3ef8d46acc259e1e22e81aa8ca5e0
Vishal Kumar Maurya <118094180+vmaurya6622@users.noreply.github.com>
2026-01-28 16:36:45 +0530

    basics Done

--- AdvancedTypes.js
+++ AdvancedTypes.js
@@ -10,8 +10,42 @@ console.log("ID as string: " + id);
 function printId(id) {
     console.log(id);
 }
-var staff = {
+var staffobj = {
     name: "SHivam",
     empId: 101
 };
-console.log("\nStaff Name: " + staff.name + ", Employee ID: " + staff.empId);
+console.log("\nStaff Name: " + staffobj.name + ", Employee ID: " + staffobj.empId);
+/*
+    Literal Types:
+    -> it allows us to specify the exact value a variable can have.
+*/
+var direction;
+var statusCode;
+statusCode = 200;
+var user = {
+    name: "Vishal",
+    age: 22
+};
+var userr = {
+    name: "Vishal"
+};
+var permissions = {
+    admin: true,
+    user: false,
+    guest: 0
+};
+console.log("\nPermissions: ", permissions);
+var productPreview = {
+    id: 1,
+    name: "Laptop",
+    price: 1000,
+    // description: "A high performance laptop" // Error: Object literal may only specify known properties, and 'description' does not exist in type 'ProductPreview'.
+};
+console.log("\nProduct Preview: ", productPreview);
+var productWithoutPrice = {
+    id: 2,
+    name: "Smartphone"
+    // price: 500 // Error: Object literal may only specify known properties, and 'price' does not exist in type 'ProductWithoutPrice'.
+};
+console.log("\nProduct Without Price: ", productWithoutPrice);
+// basically it allows us to make all properties of a type optional.
--- AdvancedTypes.ts
+++ AdvancedTypes.ts
@@ -33,3 +33,101 @@ let staffobj: Staff = {
     empId: 101
 };
 console.log("\nStaff Name: " + staffobj.name + ", Employee ID: " + staffobj.empId);
+
+/*
+    Literal Types:
+    -> it allows us to specify the exact value a variable can have.
+*/
+let direction: "left" | "right" | "up" | "down";
+
+let statusCode: 200 | 404 | 500;
+
+statusCode = 200;
+// statusCode = 401; // Error: Type '401' is not assignable to type '200 | 404 | 500'. 
+
+/*
+    Mapped Types:
+    -> it allows us to create new types based on existing ones by transforming each property in some way.
+    Builtin Types are:
+        - Partial<Type> //all optional
+        - Readonly<Type> //all readonly
+        - Record<Keys, Type> //key value pair
+        - Pick<Type, Keys> //select some properties
+        - Omit<Type, Keys> //exclude some properties
+*/
+
+type User = {
+    name: string;
+    age: number;
+};
+
+type ReadonlyUser = {
+    readonly [K in keyof User]: User[K];
+};
+
+let user: ReadonlyUser = {
+    name: "Vishal",
+    age: 22
+};
+// user.age = 23; // Error: Cannot assign to 'age' because it is a read-only property.
+
+
+
+//this is for optional
+type Userr = {
+    name: string;
+    age: number;
+};
+
+let userr: Partial<Userr> = { // here it is not not mandatory to provide all properties
+    name: "Vishal"
+};
+
+// this is for Record - fixed key value pairs
+type Roles = "admin" | "user" | "guest";
+
+type RolePermissions = Record<Roles, boolean | number>;
+
+let permissions: RolePermissions = {
+    admin: true,
+    user: false,
+    guest: 0
+};
+console.log("\nPermissions: ", permissions);
+
+/*
+    this is for pick - select some properties
+*/
+type Product = {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+};
+type ProductPreview = Pick<Product, "id" | "name" | "price">;
+
+let productPreview: ProductPreview = {  
+    id: 1,
+    name: "Laptop",
+    price: 1000,
+    // description: "A high performance laptop" // Error: Object literal may only specify known properties, and 'description' does not exist in type 'ProductPreview'.
+};
+console.log("\nProduct Preview: ", productPreview);
+
+/*
+    this is for omit - exclude some properties
+*/
+type FullProduct = {
+    id: number;
+    name: string;
+    price: number;
+}
+type ProductWithoutPrice = Omit<FullProduct, "price">; // here we are omitting price property
+
+let productWithoutPrice: ProductWithoutPrice = {    
+    id: 2,
+    name: "Smartphone"
+    // price: 500 // Error: Object literal may only specify known properties, and 'price' does not exist in type 'ProductWithoutPrice'.
+};
+console.log("\nProduct Without Price: ", productWithoutPrice);
+// basically it allows us to make all properties of a type optional.
